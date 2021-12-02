520939341f6665045031dff8c6120c2113a03aa8
claraqn <claraqn@naver.com>
2021-12-02 17:30:51 +0900

    rotate부분오류수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,6 +6,37 @@ import moment from 'moment';
 import UploadPage from '../UploadPage/UploadPage';
 import { useSelector } from 'react-redux';
 import NavBar from '../NavBar/NavBar';
+import styled from 'styled-components';
+
+const P = styled.p`
+  position: absolute;
+  width: 100%;
+  /* text-align: center; */
+  font-size: large;
+  font-weight: 300;
+  color: #735f4d;
+  opacity: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  /* font-family: Verdana, Geneva, Tahoma, sans-serif; */
+`;
+const DivPost = styled.div`
+  height: 280px;
+  margin: 0.3rem;
+  border-radius: 40px 40px 0 0;
+  background-color: white;
+  box-shadow: 5px 5px 5px 5px rgba(0, 0, 0.3, 0.3);
+  z-index: 2;
+  transition: 0.25s;
+  &:hover ${P} {
+    transition: 0.25s;
+    opacity: 1;
+  }
+  &:hover {
+    box-shadow: 5px 5px 5px 10px rgba(0, 0, 0.3, 0.3);
+  }
+`;
 
 const { Title } = Typography;
 // const { Meta } = Card;
@@ -31,37 +62,71 @@ function LandingPage() {
     console.log(post);
     return (
       <Col lg={6} md={8} xs={24} key={index}>
-        <div
-          style={{
-            height: '280px',
-            margin: '0.3rem',
-            borderRadius: '40px 40px 0 0',
-            backgroundColor: 'white',
-            boxShadow: '5px 5px 5px 5px rgba(0, 0, 0.3, 0.3)',
-          }}
-        >
+        <DivPost>
           <div style={{ textAlign: 'center', height: '150px', overflow: 'hidden', borderRadius: '40px 40px 0 0' }}>
-            <a href={`/post/${post._id}`}>
+            <a href={`/post/${post._id}`} style={{ height: '100%', position: 'relative' }}>
               <img src={post.selectedFile} alt="" width="100%" height="100%" />
+              <P>자세히 보기</P>
             </a>
           </div>
-          <div style={{ textAlign: 'center', padding: '1rem' }}>
-            <span>{post.writer.name} </span>
-            <div>제목 {post.title}</div>
-            <div>내용 {post.description}</div>
-            <div>
+          <div style={{ padding: '0.5rem' }}>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div
+                style={{
+                  borderRadius: '5px',
+                  padding: '0.2rem',
+                  flex: '1',
+                  textAlign: 'center',
+                  marginRight: '0.09rem',
+                  backgroundColor: '#4b4033',
+                  color: 'white',
+                }}
+              >
+                {post.writer.name}
+              </div>
+              <div
+                style={{
+                  border: '0.5px solid',
+                  borderRadius: '5px',
+                  padding: '0.2rem',
+                  flex: '2',
+                  textAlign: 'center',
+                  backgroundColor: '#735f4d',
+                  color: 'white',
+                }}
+              >
+                {post.title}
+              </div>
+            </div>
+            <div
+              style={{
+                border: '0.5px solid',
+                borderRadius: '5px',
+                padding: '0.2rem',
+                textAlign: 'center',
+                overflow: 'hidden',
+                whiteSpace: 'wrap',
+                height: '70px',
+                marginTop: '0.09rem',
+                backgroundColor: '#9e9075',
+                color: 'white',
+              }}
+            >
+              {post.description}
+            </div>
+            <div style={{ fontSize: '0.1rem', padding: '0.2rem', textAlign: 'center' }}>
               {moment(post.createdAt).format('Y')}년.{moment(post.createdAt).format('MM')}월.{moment(post.createdAt).format('D')}일.
-              {moment(post.createdAt).format('H')}시.{moment(post.createdAt).format('m')}분
+              {moment(post.createdAt).format('H')}:{moment(post.createdAt).format('m')}
             </div>
           </div>
-        </div>
+        </DivPost>
       </Col>
     );
   });
 
   return (
     <div style={{ width: '90%', margin: '3rem auto' }}>
-      <Title level={2}> 일긔 </Title>
+      <Title level={3}>일기를 꾸준히 써라. 그렇다면 언젠가는 일기가 너를 간직할 것이다_메이 웨스트</Title>
       <hr />
 
       <Row gutter={16} style={{ margin: '2rem auto' }}>
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,81 +1,90 @@
-import React, {useState} from 'react'
-import RightMenu from './Sections/RightMenu'
-import './Sections/Navbar.css'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import RightMenu from './Sections/RightMenu';
+import './Sections/Navbar.css';
+import styled, { keyframes } from 'styled-components';
+import animate from '../HomePage/style.module.css';
 
+const round = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 const Span = styled.span`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border: 2px solid;
-    animation: animate 5s linear infinite;
-    &:nth-child(1) {
-        border-radius: 38% 62% 64% 36% / 43% 35% 65% 57%;
-        border-color: #261201;
-    }
-    &:nth-child(2) {
-        animation-direction: reverse;
-        border-radius: 31% 49% 50% 60% / 65% 66% 34% 35%;
-        border-color: #8c704f;
-    }
-    &:nth-child(3) {
-        animation-duration: 3s;
-        border-radius: 83% 57% 76% 54% / 57% 34% 26% 43%;
-        border-color: #735f4d;
-    }
-`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 2px solid;
+  animation: ${round} 5s linear infinite;
+  &:nth-child(1) {
+    border-radius: 38% 62% 64% 36% / 43% 35% 65% 57%;
+    border-color: #261201;
+  }
+  &:nth-child(2) {
+    animation-direction: reverse;
+    border-radius: 31% 49% 50% 60% / 65% 66% 34% 35%;
+    border-color: #8c704f;
+  }
+  &:nth-child(3) {
+    animation-duration: 3s;
+    border-radius: 83% 57% 76% 54% / 57% 34% 26% 43%;
+    border-color: #735f4d;
+  }
+`;
 const Home = styled.span`
-    font-size: 0.8em;
-    color: black;
-    font-family: consolas;
-`
+  font-size: 0.8em;
+  color: black;
+  /* font-family: consolas; */
+`;
 
 function NavBar() {
-    const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
-    const showDrawer = () => {
-        setVisible(true)
-    }
+  const showDrawer = () => {
+    setVisible(true);
+  };
 
-    const onClose = () => {
-        setVisible(false)
-    }
+  const onClose = () => {
+    setVisible(false);
+  };
 
-    if (window.location.pathname === '/') {
-        return null
-    }
-    return (
-        <nav className="menu" style={{position: 'fixed', zIndex: 1, width: '100%'}}>
-            <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                <div className="menu__logo">
-                    <a href="/">
-                        <div
-                            style={{
-                                width: '60px',
-                                // marginTop: '-5px',
-                                height: '60px',
-                                display: 'flex',
-                                position: 'relative',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                flexDirection: 'column',
-                            }}
-                        >
-                            <Span />
-                            <Span />
-                            <Span />
-                            <Home>Home</Home>
-                        </div>
-                    </a>
-                </div>
-                <div>
-                    <RightMenu />
-                </div>
+  if (window.location.pathname === '/') {
+    return null;
+  }
+  return (
+    <nav className="menu" style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div className="menu__logo">
+          <a href="/">
+            <div
+              style={{
+                width: '60px',
+                // marginTop: '-5px',
+                height: '60px',
+                display: 'flex',
+                position: 'relative',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+              }}
+            >
+              <Span />
+              <Span />
+              <Span />
+              <Home>Home</Home>
             </div>
-        </nav>
-    )
+          </a>
+        </div>
+        <div>
+          <RightMenu />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
