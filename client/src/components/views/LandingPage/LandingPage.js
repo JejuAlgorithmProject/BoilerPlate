6c04a76509d226a4d1b2734e44aa00194eb070a7
claraqn <claraqn@naver.com>
2021-12-07 21:15:16 +0900

    landingpage styledcomponent로 변경

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Typography, Row } from 'antd';
+import { Col, Row } from 'antd';
 import axios from 'axios';
 import moment from 'moment';
 import UploadPage from '../UploadPage/UploadPage';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
+// styled-components를 사용한 디자인
 const P = styled.p`
   position: absolute;
   width: 100%;
@@ -33,16 +34,58 @@ const DivPost = styled.div`
     box-shadow: 5px 5px 5px 10px rgba(0, 0, 0.3, 0.3);
   }
 `;
-
-const { Title } = Typography;
+const PostList = styled.div`
+  text-align: center;
+  height: 150px;
+  overflow: hidden;
+  border-radius: 40px 40px 0 0;
+`;
+const Writer = styled.div`
+  border-radius: 5px;
+  padding: 0.2rem;
+  flex: 1;
+  text-align: center;
+  margin-right: 0.09rem;
+  background-color: #4b4033;
+  color: white;
+`;
+const Descript = styled.div`
+  border: 0.5px solid;
+  border-radius: 5px;
+  padding: 0.2rem;
+  text-align: center;
+  overflow: hidden;
+  white-space: wrap;
+  height: 70px;
+  margin-top: 0.09rem;
+  background-color: #9e9075;
+  color: white;
+`;
+const Title = styled.div`
+  border: 0.5px solid;
+  border-radius: 5px;
+  padding: 0.2rem;
+  flex: 2;
+  text-align: center;
+  background-color: #735f4d;
+  color: white;
+`;
+const Date = styled.div`
+  font-size: 0.1rem;
+  padding: 0.2rem;
+  text-align: center;
+`;
 
 function LandingPage() {
   const [Posts, setPosts] = useState([]);
   const user = useSelector((state) => state.user);
   console.log(user);
 
+  // 컴포넌트가 렌더링(화면에 그려지는 것)될 때마다 useEffect가 실행
   useEffect(() => {
+    // axios 비동기 통신을 통해 아래의 url로 http get요청
     axios.get('/api/post/getPosts').then((response) => {
+      // HTTP요청을 통해 받아온 데이터를 처리
       console.log(response);
       if (response.data.success) {
         // setPosts (보여줘야하는 posts )
@@ -53,66 +96,28 @@ function LandingPage() {
     });
   }, []);
 
+  // 받아온 데이터를 map 함수 통해 renderCard에 할당
   const renderCards = Posts.map((post, index) => {
     console.log(post);
     return (
       <Col lg={6} md={8} xs={24} key={index}>
         <DivPost>
-          <div style={{ textAlign: 'center', height: '150px', overflow: 'hidden', borderRadius: '40px 40px 0 0' }}>
+          <PostList>
             <a href={`/post/${post._id}`} style={{ height: '100%', position: 'relative' }}>
               <img src={post.selectedFile} alt="" width="100%" height="100%" />
               <P>자세히 보기</P>
             </a>
-          </div>
+          </PostList>
           <div style={{ padding: '0.5rem' }}>
             <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <div
-                style={{
-                  borderRadius: '5px',
-                  padding: '0.2rem',
-                  flex: '1',
-                  textAlign: 'center',
-                  marginRight: '0.09rem',
-                  backgroundColor: '#4b4033',
-                  color: 'white',
-                }}
-              >
-                {post.writer.name}
-              </div>
-              <div
-                style={{
-                  border: '0.5px solid',
-                  borderRadius: '5px',
-                  padding: '0.2rem',
-                  flex: '2',
-                  textAlign: 'center',
-                  backgroundColor: '#735f4d',
-                  color: 'white',
-                }}
-              >
-                {post.title}
-              </div>
+              <Writer>{post.writer.name}</Writer>
+              <Title>{post.title}</Title>
             </div>
-            <div
-              style={{
-                border: '0.5px solid',
-                borderRadius: '5px',
-                padding: '0.2rem',
-                textAlign: 'center',
-                overflow: 'hidden',
-                whiteSpace: 'wrap',
-                height: '70px',
-                marginTop: '0.09rem',
-                backgroundColor: '#9e9075',
-                color: 'white',
-              }}
-            >
-              {post.description}
-            </div>
-            <div style={{ fontSize: '0.1rem', padding: '0.2rem', textAlign: 'center' }}>
+            <Descript>{post.description}</Descript>
+            <Date>
               {moment(post.createdAt).format('Y')}년.{moment(post.createdAt).format('MM')}월.{moment(post.createdAt).format('D')}일.
               {moment(post.createdAt).format('H')}:{moment(post.createdAt).format('m')}
-            </div>
+            </Date>
           </div>
         </DivPost>
       </Col>
@@ -121,7 +126,7 @@ function LandingPage() {
 
   return (
     <div style={{ width: '90%', margin: '3rem auto' }}>
-      <Title level={3}>일기를 꾸준히 써라. 그렇다면 언젠가는 일기가 너를 간직할 것이다_메이 웨스트</Title>
+      <div style={{ fontSize: '2em' }}>일기를 꾸준히 써라. 그렇다면 언젠가는 일기가 너를 간직할 것이다_메이 웨스트</div>
       <hr />
 
       <Row gutter={16} style={{ margin: '2rem auto' }}>
